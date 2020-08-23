7a1f2f95e8ad3cdc5910a60bc54e28c7af952d0f
abhijeet <slashkirkmetallica@gmail.com>
2020-08-24 02:21:30 +0530

    add files with Router implemented and url params based api call

---
+++ src/hooks/useFilter.js
@@ -0,0 +1,38 @@
+import { getAllInfo, getFilteredInfo } from './useApi';
+
+const getYears = () => {
+	const years = [];
+	for (let year = 2006; year <= 2020; year++) {
+		years.push(year);
+	}
+	return years;
+};
+
+const getAllList = async () => {
+	const result = await getAllInfo();
+	return result;
+};
+
+const getFilteredList = async (queryString) => {
+	const result = await getFilteredInfo(queryString);
+	return result;
+};
+
+const FilterConfig = [
+	{
+		title: 'Years',
+		buttonList: getYears(),
+		key: 'launch_year'
+	},
+	{
+		title: 'Launching Success',
+		buttonList: [ true, false ],
+		key: 'launch_success'
+	},
+	{
+		title: 'Landing Success',
+		buttonList: [ true, false ],
+		key: 'land_success'
+	}
+];
+export { FilterConfig, getAllList, getFilteredList };
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { RenderApp } from './routes';
+import './index.css';
+
+ReactDOM.render(
+	<BrowserRouter>
+		<RenderApp />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
+
+serviceWorker.register();
